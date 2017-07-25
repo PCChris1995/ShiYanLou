ee3ec8893af625894829b797ab3e4364c2c0cf52
pchen95 <pchen95@126.com>
2017-07-25 13:20:09 +0800

    modified

--- student_teacher.py
+++ student_teacher.py
@@ -1,62 +1,38 @@
 #!/usr/bin/env python3
 
 import sys
-from collection import counter
+from collections import Counter
 
-type = 
+grade = sys.argv[2]
+types = sys.argv[1]
 
 class Person(object):
-    """
-    返回具有给定名称的 Person 对象
-    """
 
     def __init__(self, name):
         self.name = name
 
-    def get_grade(self):
-
-        s = counter(grade).most_common()
-
-        if type == "teacher":
-
+    def get_grade(self, grade):
+        s = Counter(grade).most_common()
+        if types  == "teacher":
             print('A: {}, B: {}, C: {}, D: {}'.format(s['A'],s['B'],s['C'],s['D']))
-
-        elif type == "student":
-
+        elif types  == "student":
             Pass = s['A'] + s['B'] + s['C']
-
             Fail = s['D']
-
             print('Pass: {}, Fail; {}'.format(Pass, Fail))
-
 class Student(Person):
-    """
-    返回 Student 对象，采用 name, branch, year 3 个参数
-    """
-
     def __init__(self, name, branch, year):
         Person.__init__(self, name)
         self.branch = branch
         self.year = year
-
-    def get_grade(self):
-        """
-        返回包含学生具体信息的字符串
-        """
-        
-
 class Teacher(Person):
-    """
-    返回 Teacher 对象，采用字符串列表作为参数
-    """
     def __init__(self, name, papers):
         Person.__init__(self, name)
         self.papers = papers
-
     def get_grade():
         return "{} teaches {}".format(self.name, ','.join(self.papers))
+print('type = {}, grade = {}'.format(sys.argv[1], sys.argv[2]))
 
-print()
+print(Person.get_grade(grade))
 
 
 
