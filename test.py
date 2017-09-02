0e14e735a56a9d0f519506958f0f721d9a5e3f9d
pchen95 <pchen95@126.com>
2017-09-02 23:42:40 +0800

    modify test.py

--- test.py
+++ test.py
@@ -10,12 +10,63 @@ letter_codes = [ord(ch) for ch in 'wasdrqWASDRQ']
 
 action_dict = dict(zip(letter_codes, action * 2))
 
+class game_field(self,):
+
+
 def main(stdscr):
+
+    state_action = {
+             'Init': init,
+             'Win': lambda: not_game('Win'),
+             'Gameover': lambda: not_game('Game'),
 
     def init():
         return 'Game'
 
     def not_game():
+        #picture the 'game over' or 'win'
+        #input action and judge reset the game or end the game\
+        
+        game_field.draw(stdscr)
+        action = get_user_action(stdscr)
+        responses = defaultdict(lambda: state)
+        responses['Restart'], responses['Exit'] = 'Init', 'Exit'
+        return responses[action]
+
+    def game():
+
+        game_field.draw(stdscr)
+        action = get_user_action(stdscr)
+        
+        if action = 'Restart':
+            return 'init'
+
+        elif action = 'Exit':
+            return 'Exit'
+
+        else game_field.move(action):
+        
+            if game_field.is_win():
+                return 'Win'
+   
+            if game_field.is_gameover():
+                return 'Gameover'
+        
+        return 'Game'
+        
+        state_action = {
+             'Init': init,
+             'Win': lambda: not_game('Win'),
+             'Gameover': lambda: not_game('Gameover'),
+             'Game': game
+            }
+
+                              
+                
+
+
+
+
         
 
 
